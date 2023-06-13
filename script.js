4a69804a83bc8adb293d08b00e7a989f219db413
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-14 00:47:40 +0800

    heropage

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-'user strict';
-
-
 // TOGGLE MENU //
 const navLinks = document.querySelector("#navLinks")
+const showMenuIcon = document.querySelector("#showMenu")
+const hideMenuIcon = document.querySelector("#hideMenu")
 
 function showMenu() {
-    navLinks.style.right = "0"
+    navLinks.style.right = "0";
 }
 
 function hideMenu(){
 navLinks.style.right ="-200px";
 }
+showMenuIcon.addEventListener("click",showMenu)
+hideMenuIcon.addEventListener("click",hideMenu)
+
+
