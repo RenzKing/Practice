26d32cd24fa766c20c7445e002766cc51e0e1d0b
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-13 19:33:02 +0800

    textbox

---
+++ script.js
@@ -0,0 +1,13 @@
+'user strict';
+
+
+// TOGGLE MENU //
+const navLinks = document.querySelector("#navLinks")
+
+function showMenu() {
+    navLinks.style.right = "0"
+}
+
+function hideMenu(){
+navLinks.style.right ="-200px";
+}
