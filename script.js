6008d4619fbe86d74dc24d00896c15c74d3332b6
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-19 01:43:51 +0800

    fixed nav section

--- script.js
+++ script.js
@@ -12,30 +12,3 @@ function hideMenu() {
 }
 showMenuIcon.addEventListener("click", showMenu);
 hideMenuIcon.addEventListener("click", hideMenu);
-
-// slider //
-$(document).ready(function () {
-  $(".customer-logos").slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    arrows: false,
-    dots: false,
-    pauseOnHover: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 4,
-        },
-      },
-      {
-        breakpoint: 520,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-    ],
-  });
-});
